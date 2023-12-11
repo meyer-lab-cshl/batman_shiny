8bc14004e2ee2ce60a676e8e423190eb57ca8294
Chapin <chapin@cshl.edu>
2023-12-11 12:18:12 -0500

    added install command for shinyWidgets and commented out prior packages that are already installed

--- final_scripts/install.R
+++ final_scripts/install.R
@@ -1,4 +1,6 @@
 #.libPaths("/opt/R/4.2.1/lib/R/library")
 .libPaths(c("/usr/lib64/R/library","/usr/share/R/library"))
 
-install.packages(c('ggplot2', 'viridis', 'tidyverse','shinyWidgets', 'plotly', 'ggalluvial', 'igraph', 'readxl'), repos='http://cran.us.r-project.org')
+#install.packages(c('ggplot2', 'viridis', 'tidyverse','shinyWidgets', 'plotly', 'ggalluvial', 'igraph', 'readxl'), repos='http://cran.us.r-project.org')
+
+install.packages("shinyWidgets")
